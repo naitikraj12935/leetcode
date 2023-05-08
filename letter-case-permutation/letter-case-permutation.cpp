33d74e31a0db89704967e8f2227ee5aee748a2bc
Naitik Raj <115349374+naitikraj12935@users.noreply.github.com>
2023-05-08 23:16:31 +0530

    Time: 4 ms (83.43%) | Memory: 11.6 MB (33.58%) - LeetSync

---
+++ letter-case-permutation/letter-case-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+void solve(string s,string op,vector<string> &ans)
+{
+    if(s.length()==0)
+    {
+        ans.push_back(op);
+        return ;
+    }
+    string op1=op;
+    string op2=op;
+    if(isalpha(s[0]))
+    {
+         op1.push_back(tolower(s[0]));
+         op2.push_back(toupper(s[0]));
+         s.erase(s.begin()+0);
+         solve(s,op1,ans);
+         solve(s,op2,ans);
+    }
+    else
+    {
+        op1.push_back(s[0]);
+        s.erase(s.begin()+0);
+         solve(s,op1,ans);
+        
+    }
+    return ;
+}
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        string op;
+        solve(s,op,ans);
+        return ans;
+        
+    }
+};
